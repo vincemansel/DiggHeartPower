26bd89283c50091e17776ea449f88770d3879433
Vince Mansel <code@vincemansel.com>
2021-08-30 01:02:21 -0700

    Add HRM_View
    For now, comment out (disable) Core Data

--- HRM_SwiftUI/HRM_SwiftUI/HRM_SwiftUIApp.swift
+++ HRM_SwiftUI/HRM_SwiftUI/HRM_SwiftUIApp.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 @main
 struct HRM_SwiftUIApp: App {
-    let persistenceController = PersistenceController.shared
+    //let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            HRM_View()
+                //.environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
---
+++ HRM_SwiftUI/HRM_SwiftUI/HRM_View.swift
@@ -0,0 +1,20 @@
+//
+//  HRM_View.swift
+//  HRM_SwiftUI
+//
+//  Created by Vince Mansel on 8/30/21.
+//
+
+import SwiftUI
+
+struct HRM_View: View {
+    var body: some View {
+        Text("Heart Rate Monitor")
+    }
+}
+
+struct HRM_View_Previews: PreviewProvider {
+    static var previews: some View {
+        HRM_View()
+    }
+}
