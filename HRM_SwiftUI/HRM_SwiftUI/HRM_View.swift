e163bd513552a77422dcfb658adeb81938e7ee06
Vince Mansel <code@vincemansel.com>
2021-08-30 22:06:56 -0700

    HRM_View: Initial UI Layout

--- HRM_SwiftUI/HRM_SwiftUI/HRM_View.swift
+++ HRM_SwiftUI/HRM_SwiftUI/HRM_View.swift
@@ -9,7 +9,64 @@ import SwiftUI
 
 struct HRM_View: View {
     var body: some View {
-        Text("Heart Rate Monitor")
+        VStack {
+            Text("DIGG Heart Rate Monitor")
+                .font(.title)
+                .foregroundColor(.blue)
+            
+            Divider()
+            
+            HStack {
+                
+                VStack {
+                    Text("Heart Rate")
+                    Text("----")
+                }
+                .padding()
+                .frame( maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, idealHeight: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                
+                Spacer()
+                
+                Divider()
+                
+                VStack {
+                    Text("Body Location")
+                    Text("----")
+                }
+                .padding()
+                .frame( maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, idealHeight: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+            .frame(height: 200)
+            
+            VStack {
+                Text("Status")
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15.0)
+                        .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                        .padding()
+                        .foregroundColor(.green)
+                    
+                    Text("Scrollable status")
+                        .italic()
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }
+            }
+
+            
+            Spacer()
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 15.0)
+                    .padding()
+                    .foregroundColor(.green)
+
+                Text("Line Graph: HR vs. Time")
+                    .italic()
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                
+            }
+        }
     }
 }
 
