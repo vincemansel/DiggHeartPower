4411be3845e2f27cf8c15654f7fc8a4f71501b17
Vince Mansel <code@vincemansel.com>
2021-08-31 00:53:57 -0700

    wip: CBCentralManagerDelegate:centralManagerDidUpdateState(_:)
    Note: How to update the Interface (viewModel) when something in the manager (Model) changes.

--- HRM_SwiftUI/HRM_SwiftUI/CoreBluetooth/CoreBluetoothManager.swift
+++ HRM_SwiftUI/HRM_SwiftUI/CoreBluetooth/CoreBluetoothManager.swift
@@ -19,14 +19,49 @@ class CoreBlueInterface: ObservableObject {
 class CoreBluetoothManager: NSObject {
   var centralManager: CBCentralManager!
 
+  var statusText: String = ""
+
   override init() {
     super.init()
     centralManager = CBCentralManager(delegate: self, queue: nil)
   }
 }
 
+// MARK: -
+// MARK: extension CoreBluetoothManager (Utilities)
+
+extension CoreBluetoothManager {
+
+  fileprivate func updateStatus(_ status: String) {
+    statusText = statusText + "\n" + status
+    //statusTextView.text = String(statusText)
+    print(status)
+  }
+  
+}
+
+// MARK: -
+// MARK: extension CoreBluetoothManager: CBCentralManagerDelegate
+
 extension CoreBluetoothManager: CBCentralManagerDelegate {
   func centralManagerDidUpdateState(_ central: CBCentralManager) {
-    //
+    switch central.state {
+      case .unknown:
+        updateStatus("central.state is .unknown")
+      case .resetting:
+        updateStatus("central.state is .resetting")
+      case .unsupported:
+        updateStatus("central.state is .unsupported")
+      case .unauthorized:
+        updateStatus("central.state is .unauthorized")
+      case .poweredOff:
+        updateStatus("central.state is .poweredOff")
+      case .poweredOn:
+        updateStatus("central.state is .poweredOn")
+        //startScan()
+      @unknown default:
+        fatalError()
+    }
+
   }
 }
