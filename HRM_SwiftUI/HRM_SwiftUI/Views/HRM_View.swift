a17ca65dabec9fe1a69afc39ed24b4b02029baca
Vince Mansel <code@vincemansel.com>
2021-08-31 09:04:51 -0700

    Refactor: Divide HRM_View into smaller subviews

--- HRM_SwiftUI/HRM_SwiftUI/Views/HRM_View.swift
+++ HRM_SwiftUI/HRM_SwiftUI/Views/HRM_View.swift
@@ -9,14 +9,12 @@ import SwiftUI
 import Charts
 
 struct HRM_View: View {
-  
   @ObservedObject var interface: CBInterface = CBInterface()
   
   @State private var statusPickerOption: StatusPickerOptions = .hrm
   @State private var hrZoneBinDivisor: Float = secondsPerMinute
   
   var body: some View {
-    
     VStack {
       Text("DIGG Heart Rate Monitor")
         .font(.title)
@@ -24,118 +22,13 @@ struct HRM_View: View {
       
       Divider()
       
-      HStack {
-        
-        VStack {
-          Text("Heart Rate")
-          Text(String(interface.heartRateReceived))
-            .font(.system(size: 56.0))
-        }
-        .padding()
-        .frame( maxWidth: .infinity, idealHeight: 200)
-        
-        Spacer()
-        
-        Divider()
-        
-        VStack {
-          Text("Body Location")
-          Text(interface.bodySensorLocation)
-            .font(.system(size: 50))
-        }
-        .padding()
-        .frame( maxWidth: .infinity, idealHeight: 200)
+      Group {
+        HeartRateParameterView()
+        ControlButtonsView()
+        StatusSelectionView(statusPickerOption: $statusPickerOption)
+        HeartRateChartsView(hrZoneBinDivisor: $hrZoneBinDivisor)
       }
-      .frame(height: 100)
-      
-      VStack (spacing: 0.0){
-        Button(action: {
-          print("Reset")
-          interface.reset()
-          interface.heartRateData.removeAll()
-        }) {
-          RoundedRectangle(cornerRadius: 10)
-            .frame(width: 100, height: 60)
-            .overlay(Text("Reset")
-                      .foregroundColor(.white))
-        }
-        .padding()
-        
-        Text("Status")
-        
-        VStack (spacing: -8.0) {
-          
-          StatusPickerView(statusPickerOption: $statusPickerOption)
-            .padding([.leading, .trailing])
-            .onChange(of: statusPickerOption) {
-              interface.changeSelection(to: $0)
-            }
-          
-          MultilineTextField(text: $interface.statusText,
-                             backgroundColor: UIColor.orange,
-                             isEditable: false)
-            .font(.headline)
-            .border(Color.gray)
-            .frame(height: 80)
-            .background(Color.orange)
-            .padding()
-        }
-      }
-      
-      VStack(spacing: -2) {
-        
-        Text("Charts")
-        
-        VStack(spacing: -30){
-          ZStack {
-            RoundedRectangle(cornerRadius: 15.0)
-              .padding()
-              .foregroundColor(.green)
-            
-            Text("HR vs. Time")
-              .italic()
-              .foregroundColor(.yellow)
-            
-            // TODO: - This is a basic line graph of the running heart rate.
-            // No legend or numerical values
-            // TODO: Note the value is a fraction resulting in FP number.
-            Chart(data: interface.heartRateData.map  { $0/255.0 })
-                .chartStyle(
-                    LineChartStyle(.quadCurve, lineColor: .blue, lineWidth: 2)
-                )
-              .padding()
-          }
-          
-          ZStack {
-            RoundedRectangle(cornerRadius: 15.0)
-              .padding()
-              .foregroundColor(.blue)
-            
-            Text("HR Zones")
-              .italic()
-              .foregroundColor(.yellow)
-            
-            Chart(data: interface.heartRateZoneData.reversed().map { $0/hrZoneBinDivisor})
-                .chartStyle(
-                  ColumnChartStyle(column: Rectangle().foregroundColor(.red).opacity(0.7), spacing: 5)
-                )
-              .padding()
-              .padding()
-          }
-          // This enables a bin to grow to hrZoneBinDivisor points before
-          // resizing
-          .onReceive(interface.$heartRateZoneData) {
-            resizeColumnHeightIfNeeded(data: $0)
-          }
-        }
-      }
-    }
-  }
-  
-  private func resizeColumnHeightIfNeeded(data: [Float]) {
-    let maxBin = data.filter { $0 > hrZoneBinDivisor }
-    if let maxZoneCount = maxBin.first, maxZoneCount >= hrZoneBinDivisor {
-      hrZoneBinDivisor *= 2
+      .environmentObject(interface)
     }
   }
   
@@ -147,5 +40,146 @@ struct HRM_View: View {
 struct HRM_View_Previews: PreviewProvider {
   static var previews: some View {
     HRM_View()
+  }
+}
+
+struct HeartRateParameterView: View {
+  @EnvironmentObject var interface: CBInterface
+  
+  var body: some View {
+    HStack {
+      
+      VStack {
+        Text("Heart Rate")
+        Text(String(interface.heartRateReceived))
+          .font(.system(size: 56.0))
+      }
+      .padding()
+      .frame( maxWidth: .infinity, idealHeight: 200)
+      
+      Spacer()
+      
+      Divider()
+      
+      VStack {
+        Text("Body Location")
+        Text(interface.bodySensorLocation)
+          .font(.system(size: 50))
+      }
+      .padding()
+      .frame( maxWidth: .infinity, idealHeight: 200)
+    }
+    .frame(height: 100)
+  }
+}
+
+struct StatusSelectionView: View {
+  @EnvironmentObject var interface: CBInterface
+  @Binding var statusPickerOption: StatusPickerOptions
+  
+  var body: some View {
+    VStack (spacing: 0.0) {
+      Text("Status")
+      
+      VStack (spacing: -8.0) {
+        
+        StatusPickerView(statusPickerOption: $statusPickerOption)
+          .padding([.leading, .trailing])
+          .onChange(of: statusPickerOption) {
+            interface.changeSelection(to: $0)
+          }
+        
+        MultilineTextField(text: $interface.statusText,
+                           backgroundColor: UIColor.orange,
+                           isEditable: false)
+          .font(.headline)
+          .border(Color.gray)
+          .frame(height: 80)
+          .background(Color.orange)
+          .padding()
+      }
+    }
+  }
+}
+
+struct HeartRateChartsView: View {
+  @EnvironmentObject var interface: CBInterface
+  @Binding var hrZoneBinDivisor: Float
+
+  var body: some View {
+    VStack(spacing: -2) {
+      
+      Text("Charts")
+      
+      VStack(spacing: -30){
+        ZStack {
+          RoundedRectangle(cornerRadius: 15.0)
+            .padding()
+            .foregroundColor(.green)
+          
+          Text("HR vs. Time")
+            .italic()
+            .foregroundColor(.yellow)
+          
+          // TODO: - This is a basic line graph of the running heart rate.
+          // No legend or numerical values
+          // TODO: Note the value is a fraction resulting in FP number.
+          Chart(data: interface.heartRateData.map  { $0/255.0 })
+            .chartStyle(
+              LineChartStyle(.quadCurve, lineColor: .blue, lineWidth: 2)
+            )
+            .padding()
+        }
+        
+        ZStack {
+          RoundedRectangle(cornerRadius: 15.0)
+            .padding()
+            .foregroundColor(.blue)
+          
+          Text("HR Zones")
+            .italic()
+            .foregroundColor(.yellow)
+          
+          Chart(data: interface.heartRateZoneData.reversed().map { $0/hrZoneBinDivisor})
+            .chartStyle(
+              ColumnChartStyle(column: Rectangle().foregroundColor(.red).opacity(0.7), spacing: 5)
+            )
+            .padding()
+            .padding()
+        }
+        // This enables a bin to grow to hrZoneBinDivisor points before
+        // resizing
+        .onReceive(interface.$heartRateZoneData) {
+          resizeColumnHeightIfNeeded(data: $0)
+        }
+      }
+    }
+  }
+  
+  private func resizeColumnHeightIfNeeded(data: [Float]) {
+    let maxBin = data.filter { $0 > hrZoneBinDivisor }
+    if let maxZoneCount = maxBin.first, maxZoneCount >= hrZoneBinDivisor {
+      hrZoneBinDivisor *= 2
+    }
+  }
+}
+
+struct ControlButtonsView: View {
+  @EnvironmentObject var interface: CBInterface
+
+  var body: some View {
+    HStack {
+      Button(action: {
+        print("Reset")
+        interface.reset()
+        interface.heartRateData.removeAll()
+      }) {
+        RoundedRectangle(cornerRadius: 10)
+          .frame(width: 100, height: 60)
+          .overlay(Text("Reset")
+                    .foregroundColor(.white))
+      }
+      .padding()
+    }
   }
 }
